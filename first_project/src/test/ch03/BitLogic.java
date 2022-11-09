e3a33b166e660f2455ed9b4489aea7286e341b26
peanutBana <diao0321@gmail.com>
2022-11-09 12:25:55 +0900

    Conditional Operator

---
+++ first_project/src/test/ch03/BitLogic.java
@@ -0,0 +1,12 @@
+package test.ch03;
+
+public class BitLogic {
+	public static void main(String[] args) {
+		System.out.println("45 & 25 = " + (45&25));
+		System.out.println("45 | 25 = " + (45|25));
+		System.out.println("45 ^ 25 = " + (45^25));
+		System.out.println("~45" + (~45));
+		System.out.println("-----------------------");
+	}
+
+}
---
+++ first_project/src/test/ch03/BitShift.java
@@ -0,0 +1,15 @@
+package test.ch03;
+
+public class BitShift {
+
+	public static void main(String[] args) {
+		int n1 = 1;
+		int res1 = n1 << 3;
+		System.out.println(res1);
+		
+		int n2 = -8;
+		int res2 = n2 >>3;
+		System.out.println(res2);
+	}
+
+}
