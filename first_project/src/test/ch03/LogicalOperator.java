413b5eb0b65b9eed503da5967c3f6e253708d40d
peanutBana <diao0321@gmail.com>
2022-11-09 11:41:09 +0900

    LogicalOperator

---
+++ first_project/src/test/ch03/LogicalOperator.java
@@ -0,0 +1,32 @@
+package test.ch03;
+//import java.util.Scanner;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		int charCode = 'A';
+		
+		if((65 <= charCode) & (charCode <= 90)) {
+			System.out.println("대문자이군요.");
+		}
+		
+		if((97 <= charCode) && (charCode <= 122)) {
+			System.out.println("소문자이군요.");
+		}
+		
+		if((48 <= charCode) && (charCode <= 57)) {
+			System.out.println("0~9 숫자이군요.");
+		}
+		
+		int value = 6;
+		
+		if((value % 2 == 0) | (value % 3 == 0)) {
+			System.out.println("2의 배수 또는 3의 배수");
+		}
+		
+		if((value % 2 == 0) || (value % 3 == 0)) {
+			System.out.println("2의 배수 또는 3의 배수");
+		}
+		
+	}
+}
