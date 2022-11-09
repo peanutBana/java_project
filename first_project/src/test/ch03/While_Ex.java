d1ce0391a345586924d420b25248ae10518b348e
peanutBana <diao0321@gmail.com>
2022-11-09 16:06:17 +0900

    While_Ex

--- first_project/src/test/ch03/While_Ex.java
+++ first_project/src/test/ch03/While_Ex.java
@@ -3,8 +3,19 @@ package test.ch03;
 public class While_Ex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		int i = 1;
+//		long sum = 0; 
+		
+//		while(i <= 100) {
+//			sum += i;
+//			i++;
+//		}
+//		System.out.println(sum);
+		
+		do {
+			i++;
+			System.out.println(i);
+		}while(i<=10);
+			
+	}	
 }
