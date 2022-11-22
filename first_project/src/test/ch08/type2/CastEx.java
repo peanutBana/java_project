04ca9b1ac15a0f4927a4e5259c14b7d413237aa3
peanutBana <diao0321@gmail.com>
2022-11-22 14:00:40 +0900

    'interface'

--- first_project/src/test/ch08/type2/CastEx.java
+++ first_project/src/test/ch08/type2/CastEx.java
@@ -3,7 +3,9 @@ package test.ch08.type2;
 public class CastEx {
 
 	public static void main(String[] args) {
-		Vehicle veh = new Bus();		
+		Vehicle veh = new Bus();
+		
+		
 	}
 	
 	public static void ride(Vehicle veh) {
