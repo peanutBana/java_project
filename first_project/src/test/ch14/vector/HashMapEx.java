5a63c4c09caa5603aec6078da3336726a7e8f538
peanutBana <diao0321@gmail.com>
2022-12-05 13:50:20 +0900

    hashMapEx

--- first_project/src/test/ch14/vector/HashMapEx.java
+++ first_project/src/test/ch14/vector/HashMapEx.java
@@ -1,7 +1,10 @@
 package test.ch14.vector;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HashMapEx {
 
@@ -15,13 +18,36 @@ public class HashMapEx {
 		map.put("양준수", 80);
 		map.put("노은비", 95);
 		
-		System.out.println(map.size());
+		System.out.println("map.size(): " + map.size());
 		
 		//키로 값 얻기
 		String key = "김하서";
 		int value = map.get(key);
-		System.out.println(value);
+		System.out.println("value: " + value);
 	
+		//반복해서 키와 값 얻기
+		Set<String> keySet = map.keySet();
+		Iterator<String> keyInterator = keySet.iterator();
+		
+		while(keyInterator.hasNext()) {
+			String k = keyInterator.next();		// 4개의 key
+			Integer v = map.get(k);
+			System.out.println(k + " : " + v);
+		}
+		
+		System.out.println("--------------------------------------");
+		
+		//반복해서 키와 값 얻기 2번째
+		Set<Entry<String, Integer>> entrySet = map.entrySet();
+		Iterator<Entry<String, Integer>> entryIterator = entrySet.iterator();	//반복된 키를 얻는다.
+		
+		while(entryIterator.hasNext()) {
+			Entry<String, Integer> entry = entryIterator.next();
+			String k = entry.getKey();
+			Integer v = entry.getValue();
+			System.out.println(k + " : " + v);
+		}
+		
 	}
 
 }
---
+++ first_project/src/test/ch14/vector/TreeSetEx.java
@@ -0,0 +1,50 @@
+package test.ch14.vector;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSetEx {
+
+	public static void main(String[] args) {
+		TreeSet<Integer> scores = new TreeSet<>();
+		
+		scores.add(7);
+		scores.add(4);
+		scores.add(1);
+		scores.add(5);
+		scores.add(9);
+		
+		for(Integer s : scores) {
+			System.out.print(s + " " );	//자동 오름차순 정렬됨
+		}
+		System.out.println();
+		
+		System.out.println("가장 작은 값: " + scores.first());
+		System.out.println("가장 큰 값: " + scores.last());
+		System.out.println("5보다 작은 값: " + scores.lower(5));
+		System.out.println("5보다 큰 값: " + scores.higher(5));
+		
+		System.out.println();
+		//내림차순
+		NavigableSet<Integer> desc = scores.descendingSet();
+		for(Integer s : desc) {
+			System.out.println(s);
+		}
+		
+		System.out.println();
+		
+		//범위 검색(5 <= )
+		//tailSet(값, boolean) : 주어진 값보다 높은 값을 가져온다.	true : 해당 값 포함 / false : 미포함
+		NavigableSet<Integer> rangeSet = scores.tailSet(5, true);
+		for(Integer s : rangeSet) {
+			System.out.print(s + " " );
+		}
+		
+		//범위 검색(4 <= score < 9)
+		
+		
+		
+	}
+
+	
+}
